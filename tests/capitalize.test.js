9ab12dedb00be7eb91214b31f2472190805dac4c
Daniil <kuzzmy@yandex.ru>
2025-12-28 17:13:44 +0300

    method - assert.equal

--- tests/capitalize.test.js
+++ tests/capitalize.test.js
@@ -1,12 +1,15 @@
+import { strict as assert } from 'node:assert'
+// при использовании strict-режима
+// проверка equal равносильна strictEqual
+
 import capitalize from '../src/capitalize.js'
+console.log ("Lests GO")
+// Проверка сменилась с отрицательной на положительную
+assert.equal(capitalize(''), '')
+// Первый параметр actual – то, что пришло
+// Второй параметр expected – то, что ожидает тест
+// Правильный порядок аргументов имеет большое значение при анализе ошибки
 
-if (capitalize('Hello') !== 'Hello') {
-  throw new Error('Функция работает неверно!')
-}
+assert.equal(capitalize('hello'), 'Hello')
 
-if (capitalize('') !== '') {
-  throw new Error('Функция работает неверно!')
-  
-}
-
-console.log('Все тесты пройдены!')
+console.log("FINAL TEST")
